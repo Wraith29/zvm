c3613e38ff5d5bb411969ad629c70bb6f885ab97
Isaac Naylor <i.acnaylor@gmail.com>
2023-04-28 08:55:17 +0100

    moved `path.zig` to `_Path.zig`

---
+++ src/_Path.zig
@@ -0,0 +1,60 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const Path = @This();
+
+allocator: Allocator,
+base_path: []const u8,
+
+fn getSubpath(allocator: Allocator, base: []const u8, child: []const u8) ![]const u8 {
+    return std.mem.concat(allocator, u8, &[_][]const u8{
+        base,
+        std.fs.path.sep_str,
+        child,
+    });
+}
+
+fn pathExists(path: []const u8) bool {
+    std.fs.accessAbsolute(path, .{}) catch return false;
+    return true;
+}
+
+pub fn openFile(file_path: []const u8, flags: std.fs.File.OpenFlags) !std.fs.File {
+    if (!pathExists(file_path)) {
+        return try std.fs.createFileAbsolute(file_path, .{ .read = true });
+    }
+
+    return try std.fs.openFileAbsolute(file_path, flags);
+}
+
+pub fn init(allocator: Allocator) !Path {
+    var base = try std.fs.getAppDataDir(allocator, ".zvm");
+    if (!pathExists(base)) try std.fs.makeDirAbsolute(base);
+
+    return Path{
+        .allocator = allocator,
+        .base_path = base,
+    };
+}
+
+pub fn deinit(self: *Path) void {
+    self.allocator.free(self.base_path);
+}
+
+pub fn getVersionPath(self: *Path, version: []const u8) ![]const u8 {
+    return try getSubpath(self.allocator, self.toolchain_path, version);
+}
+
+pub fn getTmpVersionPath(self: *Path, version: []const u8) ![]const u8 {
+    var tmp_version = try std.mem.concat(self.allocator, u8, &[_][]const u8{ "tmp-", version });
+
+    return try self.getVersionPath(tmp_version);
+}
+
+pub fn getToolchainPath(self: *Path) ![]const u8 {
+    return try getSubpath(self.allocator, self.base_path, "toolchains");
+}
+
+pub fn getCachePath(self: *Path) ![]const u8 {
+    return try getSubpath(self.allocator, self.base_path, "cache.json");
+}
