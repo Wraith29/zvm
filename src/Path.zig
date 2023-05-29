c22bdd700b98a7b9659b7a7e667b5d038c755eb3
Isaac Naylor <i.acnaylor@gmail.com>
2023-05-29 19:24:35 +0100

    fixed (I think) the env var issue

--- src/Path.zig
+++ src/Path.zig
@@ -61,6 +61,7 @@ pub fn setup(self: *const Path) !void {
     if (builtin.os.tag == .windows) {
         var command_string = std.ArrayList(u8).init(self.allocator);
 
+        std.log.info("Creating Env Var Command", .{});
         try std.fmt.format(
             command_string.writer(),
             "{{[System.Environment]::SetEnvironmentVariable(\"ZIG_PATH\", \"{s}\", \"User\")}}",
@@ -69,10 +70,18 @@ pub fn setup(self: *const Path) !void {
 
         var cmd = try command_string.toOwnedSlice();
 
-        _ = try std.ChildProcess.exec(.{
+        std.log.info("Executing Env Var Command", .{});
+        var env_var_set_result = try std.ChildProcess.exec(.{
             .allocator = self.allocator,
             .argv = &[_][]const u8{ "pwsh", "-Command", cmd },
         });
+
+        std.log.info("stdout: {s}", .{env_var_set_result.stdout});
+
+        std.log.info("{any}", .{env_var_set_result});
+        if (env_var_set_result.stderr.len != 0) {
+            std.log.err("Error Updating Env Var, {s}", .{env_var_set_result.stderr});
+        }
     }
 }
 
